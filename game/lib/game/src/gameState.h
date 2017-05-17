03e25219135ed8ede7677dd9015ffd01a68dd6d7
Rick van Schijndel <rol3517@gmail.com>
2017-05-17 16:14:21 +0200

    Added gravity/jump logic, created some screens, made pseudocode for collision detection
    Made some global game state values, like score and y pos and velocity

--- game/lib/game/src/gameState.h
+++ game/lib/game/src/gameState.h
@@ -4,10 +4,3 @@ enum gameState {
   play,
   gameOver
 };
-
-
-gameState game_state = start;
-
-inline void nextGameState() {
-  game_state = static_cast<gameState>((game_state + 1) % (gameOver + 1));
-}
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <stdio.h>
 
 #include <Bounce2.h>
 #include <U8g2lib.h>
@@ -18,6 +19,16 @@ const int buttonPin = D3;
 const int screenWidth = 128;
 const int screenHeight = 64;
 
+// for the jumping algorithm
+const int gravity = 1;
+const int jump_velocity = 10;
+
+// global game state
+gameState game_state = start;
+int16_t player_y_position = 0; // 0 is on the ground, negative is invalid
+int16_t player_y_velocity = 0; // positive is going up, negative is gravity
+uint32_t distance_traveled = 0; // start at 0, represents score
+
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
 Bounce button;
@@ -31,19 +42,92 @@ void setup(void) {
   Serial.begin(115200);
 }
 
+inline void nextGameState() {
+  game_state = static_cast<gameState>((game_state + 1) % (gameOver + 1));
+}
+
 void drawPlayer(int y_position = 0)
 {
   int y_position_from_beneath = screenHeight - player_height - y_position ;
   u8g2.drawXBM(((screenWidth / 3) - (player_width / 2)), y_position_from_beneath, player_width, player_height, player_bits);
 }
 
-void drawBootupscreen(void)
+void drawObstacles() {
+  // TODO: draw obstacles according to the values in some array
+}
+
+
+void drawBootupScreen(void)
 {
   int logoMiddleHorizontal = (screenWidth / 2) - (bootup_width / 2);
   int logoMiddleVertical = (screenHeight / 2) - (bootup_height / 2);
   u8g2.drawXBM(logoMiddleHorizontal, logoMiddleVertical, bootup_width, bootup_height, bootup_bits);
 }
 
+const int x_pos_gameover = 40;
+const int y_pos_gameover = 20;
+void drawGameOver(void) {
+  u8g2.drawStr(x_pos_gameover, y_pos_gameover, "GAME OVER");
+  // TODO: draw fancy asset?
+}
+
+const int x_pos_score = 80;
+const int y_pos_score = 20;
+const size_t score_buffer_size = 20;
+void drawScore(void) {
+  char scorebuffer[score_buffer_size];
+  snprintf(scorebuffer, score_buffer_size, "Score: %u", distance_traveled);
+  u8g2.drawStr(x_pos_score, y_pos_score, scorebuffer);
+}
+
+void drawHiscoreScreen(void) {
+  // TODO: draw hi scores
+}
+
+bool collisionDetected(void) {
+  /*
+  if player_right_x_position >= obstacle_left_x_position and
+    player_left_x_position <= obstacle_right_x_position
+    player_bottom_y_position <= obstacle_top_y_position
+  */
+
+  // TODO: implement algorithm, remove random
+  return random(1000) == 321;
+}
+
+inline bool playerOnGround(void) {
+  return player_y_position == 0;
+}
+
+void updatePlayerYPosition(void) {
+  // player stops falling when ground is hit
+  if (playerOnGround() && player_y_velocity <= 0) {
+    player_y_velocity = 0;
+    return;
+  }
+  else if (player_y_velocity != 0) {
+    // increment height with velocity, also if velocity is negative (falling)
+    player_y_position += player_y_velocity;
+    if (player_y_position < 0) {
+      player_y_position = 0;
+    }
+    // subtract gravity from velocity
+    player_y_velocity -= gravity;
+  }
+}
+
+void updateObstaclePosition(void) {
+  // TODO
+}
+
+void updateScore(void) {
+  distance_traveled += 1;
+}
+
+// unconditional jump ;-)
+void jump(void) {
+  player_y_velocity = jump_velocity;
+}
 
 void loop(void) {
   // in the loop so it's only true once, after this it's false again.
@@ -56,13 +140,14 @@ void loop(void) {
     Serial.println("Button update");
   }
 
+  // clear buffer to start drawing
   u8g2.clearBuffer();
 
   switch (game_state)
   {
     case start:
       {
-        drawBootupscreen();
+        drawBootupScreen();
         if (buttonPressed) {
           nextGameState();
         }
@@ -70,6 +155,7 @@ void loop(void) {
       }
     case hiscore:
       {
+        drawHiscoreScreen();
         if (buttonPressed) {
           nextGameState();
         }
@@ -77,28 +163,41 @@ void loop(void) {
       }
     case play:
       {
-        int player_position = 0;
         if (buttonPressed) {
-          static int timesPressed = 0;
-          timesPressed = timesPressed + 1;
-          if (timesPressed == 10) {
-            timesPressed = 0;
-            nextGameState();
+          if (playerOnGround()) {
+            jump();
           }
-          player_position = 10;
         }
-        drawPlayer(player_position);
 
+        updatePlayerYPosition();
+        updateObstaclePosition();
+        drawPlayer(player_y_position);
+        drawObstacles();
+
+        if (collisionDetected()) {
+          nextGameState();
+          break;
+        }
+        // happens after collision detection, so score will only get higher
+        // if player does not collide
+        updateScore();
         break;
       }
     case gameOver:
       {
+        drawGameOver();
+        drawScore();
         if (buttonPressed) {
           nextGameState();
         }
         break;
       }
+    default:
+      {
+        Serial.println("Invalid game state");
+        break;
+      }
   }
-
+  // draw everything on the screen
   u8g2.sendBuffer();
 }
