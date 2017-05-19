1b85bc9c3957a5fe5c20ddbd15345e25a9efe7a6
Rick van Schijndel <rol3517@gmail.com>
2017-05-19 21:39:50 +0200

    Moved game state to new folder

---
+++ game/lib/state/game_state.h
@@ -0,0 +1,11 @@
+#ifndef GAME_LIB_STATE_GAME_STATE_H_
+#define GAME_LIB_STATE_GAME_STATE_H_
+
+enum gameState {
+  start,
+  hiscore,
+  play,
+  gameOver
+};
+
+#endif  // GAME_LIB_STATE_GAME_STATE_H_
