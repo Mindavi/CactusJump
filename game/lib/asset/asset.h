5f4a6a69992ca6f36368bdbc492e5ab1dd6b83fc
Rick van Schijndel <rol3517@gmail.com>
2017-05-28 18:00:34 +0200

    Added something so assets can draw themselves

--- game/lib/asset/asset.h
+++ game/lib/asset/asset.h
@@ -2,6 +2,7 @@
 #define GAME_LIB_ASSET_ASSET_H_
 
 #include <cinttypes>
+#include <U8g2lib.h>
 
 class Asset {
  public:
@@ -9,6 +10,9 @@ class Asset {
   uint16_t getWidth();
   uint16_t getHeight();
   unsigned char* getBitmap();
+  void Draw(uint16_t x,
+    uint16_t y,
+    U8G2_SSD1306_128X64_NONAME_F_HW_I2C* renderer);
  private:
   uint16_t m_width;
   uint16_t m_height;
